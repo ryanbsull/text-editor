8b436d294eb784a09db79de0308a94e5229884db
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-05-08 17:14:28 -0400

    t-edit: add in check to make sure file exists to edit
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- editor.c
+++ editor.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) exit(1);
+  if (argc < 2 || access(argv[1], F_OK)) return 1;
 
   for (int i = 0; i < argc - 1; i++) printf("%s, ", argv[i]);
   printf("%s\n", argv[argc - 1]);
