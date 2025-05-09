ebb9609466a3beadf6c3be0d7eaefc7541e2480e
Ryan B. Sullivan <ryanbakersullivan@gmail.com>
2025-05-09 13:29:35 -0400

    t-edit: accept multiple files and check existence
    Signed-off-by: Ryan B. Sullivan <ryanbakersullivan@gmail.com>

--- editor.c
+++ editor.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int verify(int argc, char *argv[]) {
+  for (int i = 1; i < argc; i++) {
+    if (access(argv[i], F_OK)) return 1;
+  }
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
-  if (argc < 2 || access(argv[1], F_OK)) return 1;
+  if (argc < 2 || verify(argc, argv)) return 1;
 
   for (int i = 0; i < argc - 1; i++) printf("%s, ", argv[i]);
   printf("%s\n", argv[argc - 1]);
